7ad15b903fbd07e7bd0ce5e9f85365bf2d04dcba
Romulo P T Mota <romuloptmota@gmail.com>
2023-01-29 15:15:11 -0300

    Criado model para serviços

--- core/models.py
+++ core/models.py
@@ -12,3 +12,22 @@ class Base(models.Model):
         abstract = True
 
 
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-stats-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+    servico = models.CharField('Serviço', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    icone = models.CharField('Icone', max_length=12, choices=ICONE_CHOICES)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+    def __str__(self):
+        return self.servico
