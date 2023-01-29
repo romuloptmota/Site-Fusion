89a61a7203cabec3f117e33ef7af27db95d50dda
Romulo P T Mota <romuloptmota@gmail.com>
2023-01-29 17:29:01 -0300

    configurado a views para manipuar as informações do banco e enviar ao template

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from.models import Servico, Funcionario
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context['servicos'] = Servico.objects.all()
+        context['funcionarios'] = Funcionario.objects.all()
+        return context
