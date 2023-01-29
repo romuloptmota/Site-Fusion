291aef1dc1abf525bfd43d2d0953428e4a9003e6
Romulo P T Mota <romuloptmota@gmail.com>
2023-01-29 15:09:00 -0300

    Criado base dos models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from stdimage.models import StdImageField
+
+
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+
