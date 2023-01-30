040a5169a865abc977d13954c7b75f27bd618660
Romulo P T Mota <romuloptmota@gmail.com>
2023-01-30 02:10:57 -0300

    Corrigindo settings debug = false

--- fusion/settings.py
+++ fusion/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rmd)f68u_2$5=+&@-lv@pcqzde=j4u0u-#7$q)w+6ujbvljm44'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
